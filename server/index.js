75c9420b915c8feb2062aafd10af6163f6bcf71a
Daniel Xiao <danielxiao@Daniels-MacBook-Air.local>
2022-04-04 20:19:29 -0500

    Changed .env file

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_DATABASE = process.env.DB_DATABASE
 const DB_PORT = process.env.DB_PORT
-const db = mysql.createPool({
+const db = mysql.createPool({ 
   connectionLimit: 100,
   host: DB_HOST,
   user: DB_USER,
