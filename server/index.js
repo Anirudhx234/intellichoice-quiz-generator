d28edb594c9b6846866556732bb1eef10d8ee599
Timothy Haynes <timothyihaynes@gmail.com>
2022-04-14 15:15:51 -0500

    server connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,16 +1,10 @@
+/*
+
 const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
-
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -24,9 +18,8 @@ app.use((req, res, next) => {
 
 app.listen(port, () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
+
   console.log(`Server is running on port: ${port}`);
 });
+
+*/
