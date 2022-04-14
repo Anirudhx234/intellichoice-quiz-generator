153b41fb6b316d91ce58bc843e68ee1f6e59a1fe
Timothy Haynes <timothyihaynes@gmail.com>
2022-04-14 14:46:47 -0500

    index.js setup and client node modules

--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -15,5 +23,10 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });
